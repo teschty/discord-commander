621b2ac41f8e1a4aa58adfbfe9322a41c426f13c
Tyler Tesch <tytesch@gmail.com>
2018-07-07 19:43:13 -0500

    Agh, add Flags to export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import "reflect-metadata";
 
 export { CommandClient as Client } from "./client";
 export { command, rest, optional, checks } from "./decorators";
-export { Context } from "./command";
+export { Context, Flags } from "./command";
 export * from "discord.js";
